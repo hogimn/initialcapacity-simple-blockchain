fcdcaf334896fb331178be28ca060e5be2be661e
hogimn <hogimn@gmail.com>
2023-05-26 13:06:38 +0900

    Fill in TODOs of initialcapacity-simple-blockchain

--- src/main/java/io/collective/basic/Block.java
+++ src/main/java/io/collective/basic/Block.java
@@ -4,41 +4,93 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Represents a block in a blockchain.
+ */
 public class Block {
+
+    // Hash of the previous block
     private String previousHash;
+
+    // Timestamp of the block
     private long timestamp;
+
+    // Nonce value used in mining
     private int nonce;
+
+    // Hash value of the block
     private String hash;
 
+    /**
+     * Constructs a new block with the given parameters.
+     *
+     * @param previousHash The hash of the previous block.
+     * @param timestamp The timestamp of the block.
+     * @param nonce The nonce value used in mining.
+     * @throws NoSuchAlgorithmException If the SHA-256 algorithm is not available.
+     */
     public Block(String previousHash, long timestamp, int nonce) throws NoSuchAlgorithmException {
         this.previousHash = previousHash;
         this.timestamp = timestamp;
         this.nonce = nonce;
-        this.hash = null;
+        this.hash = calculatedHash(); // Calculate the hash value for the block
     }
 
+    /**
+     * Returns the hash of the previous block.
+     *
+     * @return The hash of the previous block.
+     */
     public String getPreviousHash() {
         return previousHash;
     }
 
+    /**
+     * Returns the timestamp of the block.
+     *
+     * @return The timestamp of the block.
+     */
     public long getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * Returns the nonce value used in mining.
+     *
+     * @return The nonce value used in mining.
+     */
     public int getNonce() {
         return nonce;
     }
 
+    /**
+     * Returns the hash value of the block.
+     *
+     * @return The hash value of the block.
+     */
     public String getHash() {
         return hash;
     }
 
+    /**
+     * Calculates the hash value for the block.
+     *
+     * @return The calculated hash value for the block.
+     * @throws NoSuchAlgorithmException If the SHA-256 algorithm is not available.
+     */
     public String calculatedHash() throws NoSuchAlgorithmException {
-        return null;
+        return calculateHash(previousHash + timestamp + nonce);
     }
 
     /// Supporting functions that you'll need.
 
+    /**
+     * Calculates the SHA-256 hash value of a string.
+     *
+     * @param string The input string.
+     * @return The calculated SHA-256 hash value.
+     * @throws NoSuchAlgorithmException If the SHA-256 algorithm is not available.
+     */
     static String calculateHash(String string) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         digest.update(string.getBytes());
--- src/main/java/io/collective/basic/Blockchain.java
+++ src/main/java/io/collective/basic/Blockchain.java
@@ -1,42 +1,127 @@
 package io.collective.basic;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * Represents a blockchain that stores a sequence of blocks.
+ */
 public class Blockchain {
 
+    /**
+     * List to store the blocks in the blockchain
+     */
+    private final List<Block> mBlocks;
+
+    /**
+     * Constructs an empty blockchain.
+     */
+    public Blockchain() {
+        mBlocks = new LinkedList<>();
+    }
+
+    /**
+     * Checks if the blockchain is empty.
+     *
+     * @return true if the blockchain is empty, false otherwise.
+     */
     public boolean isEmpty() {
-        return false;
+        return mBlocks.isEmpty();
     }
 
+    /**
+     * Adds a block to the blockchain.
+     *
+     * @param block The block to be added.
+     */
     public void add(Block block) {
+        mBlocks.add(block);
     }
 
+    /**
+     * Returns the size of the blockchain.
+     *
+     * @return The number of blocks in the blockchain.
+     */
     public int size() {
-        return 0;
+        return mBlocks.size();
     }
 
+    /**
+     * Checks if the blockchain is valid.
+     *
+     * @return true if the blockchain is valid, false otherwise.
+     * @throws NoSuchAlgorithmException if the hashing algorithm is not available.
+     */
     public boolean isValid() throws NoSuchAlgorithmException {
 
         // todo - check an empty chain
+        if (mBlocks.isEmpty())
+            return true;
 
         // todo - check a chain of one
+        else if (mBlocks.size() == 1) {
+            Block firstBlock = mBlocks.get(0);
+            return isMined(firstBlock) && firstBlock.getHash().equals(firstBlock.calculatedHash());
+        }
 
         // todo - check a chain of many
+        else {
+            Block currentBlock;
+            Block previousBlock;
 
-        return false;
+            // Iterate through each block in the blockchain
+            for (int i = 1; i < mBlocks.size(); i++) {
+                currentBlock = mBlocks.get(i);
+                previousBlock = mBlocks.get(i - 1);
+
+                // Check if the current block and the previous block are mined
+                if (!isMined(currentBlock) || !isMined(previousBlock))
+                    return false;
+
+                // Check if the current block's hash is valid
+                if (!currentBlock.getHash().equals(currentBlock.calculatedHash()))
+                    return false;
+
+                // Check if the previous block's hash is valid
+                if (!previousBlock.getHash().equals(previousBlock.calculatedHash()))
+                    return false;
+
+                // Check if the current block's previous hash matches the previous block's hash
+                if (!currentBlock.getPreviousHash().equals(previousBlock.getHash()))
+                    return false;
+            }
+        }
+
+        return true;
     }
 
     /// Supporting functions that you'll need.
 
+    /**
+     * Mines a block by finding a nonce that satisfies the mining condition.
+     *
+     * @param block The block to be mined.
+     * @return The mined block.
+     * @throws NoSuchAlgorithmException if the hashing algorithm is not available.
+     */
     public static Block mine(Block block) throws NoSuchAlgorithmException {
         Block mined = new Block(block.getPreviousHash(), block.getTimestamp(), block.getNonce());
 
         while (!isMined(mined)) {
             mined = new Block(mined.getPreviousHash(), mined.getTimestamp(), mined.getNonce() + 1);
         }
+
         return mined;
     }
 
+    /**
+     * Checks if a block is mined by checking its hash.
+     *
+     * @param minedBlock The block to be checked.
+     * @return true if the block is mined, false otherwise.
+     */
     public static boolean isMined(Block minedBlock) {
         return minedBlock.getHash().startsWith("00");
     }
